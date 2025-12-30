3fc3eec0eedd71d99a585eb49232520eadd8f8cb
[오종석][기술개발본부][사원] <ojs6889@datau.co.kr>
2025-12-30 11:43:37 +0900

    action 실패 조건 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     id("io.gitlab.arturbosch.detekt")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -62,6 +63,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(platform("com.google.firebase:firebase-bom:34.7.0"))
 }
 
 detekt {
@@ -70,4 +73,5 @@ detekt {
     buildUponDefaultConfig = true
     allRules = false
     autoCorrect = true
+    ignoreFailures = false
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,5 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
     id("io.gitlab.arturbosch.detekt") version "1.19.0" apply false
+    id("com.google.gms.google-services") version "4.4.4" apply false
 }
