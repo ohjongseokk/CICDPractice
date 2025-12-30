fb2da64358afffefc53178882e9f1537e3eabf0f
[오종석][기술개발본부][사원] <ojs6889@datau.co.kr>
2025-12-30 10:56:00 +0900

    BaseUrlRule 추가

---
+++ detekt/src/main/java/co/kr/datau/detekt/rules/BaseUrlRule.kt
@@ -0,0 +1,35 @@
+package co.kr.datau.detekt.rules
+
+import io.gitlab.arturbosch.detekt.api.*
+import org.jetbrains.kotlin.psi.*
+
+class BaseUrlRule(config: Config) : Rule(config) {
+    override val issue = Issue(
+        id = "CheckBaseUrl",
+        severity = Severity.CodeSmell,
+        description = "BASE_URL is contain dev",
+        debt = Debt.FIVE_MINS
+    )
+
+    override fun visitProperty(property: KtProperty) {
+        super.visitProperty(property)
+
+        if (
+            property.name == "BASE_URL" &&
+            property.isTopLevel &&
+            property.containingKtFile.packageFqName.asString() == "co.kr.datau.cicdpractice"
+        ) {
+            val initializer = property.initializer?.text
+
+            if (initializer?.contains("dev") == true) {
+                report(
+                    CodeSmell(
+                        issue,
+                        Entity.from(property),
+                        "BaseUrl must not contain dev, but found $initializer"
+                    )
+                )
+            }
+        }
+    }
+}
---
+++ detekt/src/main/java/co/kr/datau/detekt/rules/provider/ConstRuleSetProvider.kt
@@ -0,0 +1,19 @@
+package co.kr.datau.detekt.rules.provider
+
+import co.kr.datau.detekt.rules.BaseUrlRule
+import co.kr.datau.detekt.rules.ConstValueRule
+import io.gitlab.arturbosch.detekt.api.*
+
+class ConstRuleSetProvider : RuleSetProvider {
+    override val ruleSetId: String = "const-rules"
+
+    override fun instance(config: Config): RuleSet {
+        return RuleSet(
+            ruleSetId,
+            listOf(
+                ConstValueRule(config),
+                BaseUrlRule(config)
+            )
+        )
+    }
+}
