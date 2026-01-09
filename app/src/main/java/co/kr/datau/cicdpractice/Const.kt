aeea6c7d4b60919f803a9527bab5e4bd15831b4d
[오종석][기술개발본부][사원] <ohjong6889@datau.co.kr>
2026-01-09 09:48:42 +0900

    [build] android.yml 수정 및 BASE_URL 상용기로 변경

--- app/src/main/java/co/kr/datau/cicdpractice/Const.kt
+++ app/src/main/java/co/kr/datau/cicdpractice/Const.kt
@@ -2,7 +2,7 @@ package co.kr.datau.cicdpractice
 
 object Const {
 //    const val BASE_URL = BuildConfig.BASE_URL
-    const val BASE_URL = "dev-www.naver.com"
+    const val BASE_URL = "www.naver.com"
 
     const val RELEASE_VALUE_INT = 0
     const val RELEASE_VALUE_STRING = ""
